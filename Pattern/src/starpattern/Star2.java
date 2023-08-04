bb1ad6c7d27ae8b0eceb247cd55f487075a00892
Thilagaraj R <138859070+Thilagarajdinesh@users.noreply.github.com>
2023-08-04 15:44:50 +0530

    Add files via upload

---
+++ Pattern/src/starpattern/Star2.java
@@ -0,0 +1,17 @@
+package starpattern;
+
+public class Star2 {
+
+	public static void main(String[] args) {
+	
+		for(int i=0;i<=5;i++) {
+			for(int j=5-1;j>=i;j--) {
+				System.out.print(" ");
+			}
+		for(int k=1;k<=i;k++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+
+}}
